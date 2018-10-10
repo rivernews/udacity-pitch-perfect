f75db08388b19e314c4a1dc2e13d3aeee726c428
Shaung Cheng <shaungc@umich.edu>
2018-10-10 00:28:07 -0400

    Initial Commit

---
+++ udacity-pitch-perfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udacity-pitch-perfect
+//
+//  Created by Shaung Cheng on 10/10/18.
+//  Copyright © 2018 Udacity Shaung Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
