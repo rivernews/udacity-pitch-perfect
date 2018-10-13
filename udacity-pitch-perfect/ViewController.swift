586e75ba0c2f198ba2c9db5eb0828ba0e9e3fd5a
Shaung Cheng <shaungc@umich.edu>
2018-10-13 09:51:31 -0400

    save

--- udacity-pitch-perfect/ViewController.swift
+++ udacity-pitch-perfect/ViewController.swift
@@ -11,10 +11,21 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear completed!")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     @IBAction func recordAudio(_ sender: Any) {
